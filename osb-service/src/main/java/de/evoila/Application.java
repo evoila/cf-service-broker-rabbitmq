f16a21baf393465dfa7ac011218701750e5ad619
Johannes Hiemer <jhiemer@evoila.de>
2018-08-23 15:37:42 +0200

    Updating Spring Boot Version

--- osb-service/src/main/java/de/evoila/Application.java
+++ osb-service/src/main/java/de/evoila/Application.java
@@ -4,44 +4,38 @@
 package de.evoila;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.cloud.bus.BusAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
+ *
  * @author Johannes Hiemer.
  *
  */
 @RefreshScope
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { RabbitAutoConfiguration.class, BusAutoConfiguration.class })
 public class Application {
 
-	@Bean(name = "customProperties")
-	public Map<String, String> customProperties() {
-		Map<String, String> customProperties = new HashMap<String, String>();
+    @Bean(name = "customProperties")
+    public Map<String, String> customProperties() {
+        Map<String, String> customProperties = new HashMap<String, String>();
 
-		return customProperties;
-	}
+        return customProperties;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(Application.class);
-		springApplication.addListeners(new ApplicationPidFileWriter());
-		ApplicationContext ctx = springApplication.run(args);
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        ApplicationContext ctx = springApplication.run(args);
 
-		Assert.notNull(ctx, "ApplicationContext must not be null.");
-		
-	}
+        Assert.notNull(ctx, "ApplicationContext must not be null.");
+    }
 
 }
